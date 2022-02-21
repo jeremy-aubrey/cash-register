6256a02aa0902a423171e8cef3e2b770de610cf1
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-21 15:12:24 -0600

    implemented getInventoryFile method

--- src/project3/TestProject3.java
+++ src/project3/TestProject3.java
@@ -1,11 +1,53 @@
 package project3;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class TestProject3 {
 	public static void main(String[] args) {
 		
 		TestProject3 obj = new TestProject3();
 		obj.developerInfo();
 		
+		File inventoryFile = obj.getInventoryFile();
+		
+		CashRegister register = new CashRegister();
+		register.displayMenu();
+	}
+	
+	public File getInventoryFile() {
+		
+		File inventoryFile = null;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an inventory file path:");
+		Path path = null;
+	
+		//get legal path
+		try {
+			path = Paths.get(input.nextLine().trim());
+			boolean exists = Files.exists(path);
+			boolean isReadable = Files.isReadable(path);
+			boolean isDirectory = Files.isDirectory(path);
+			
+			//instantiate File if its readable
+			if(exists && isReadable && !isDirectory) {
+				inventoryFile = new File(path.toString());
+				System.out.println("Using: " + path);
+			} else {
+				System.out.println("Unable to read file");
+			}
+			
+		} catch (InvalidPathException | SecurityException | NullPointerException e) {
+			System.out.println("Illegal path:");
+			System.out.println(e.getMessage());
+		}
+		
+		input.close();
+		return inventoryFile;
 	}
 	
    //***************************************************************
@@ -19,8 +61,8 @@ public class TestProject3 {
    //  Returns:      N/A 
    //
    //**************************************************************
-   public void developerInfo()
-   {
+   public void developerInfo() {
+	   
       System.out.println("Name:    Jeremy Aubrey");
       System.out.println("Course:  COSC 4301 Modern Programming");
       System.out.println("Project: Three\n");
