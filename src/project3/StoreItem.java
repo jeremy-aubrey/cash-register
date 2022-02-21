c1a02b86535d76f886cdb727fd378958a55b6ce7
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-21 12:47:11 -0600

    implemented constructor

--- src/project3/StoreItem.java
+++ src/project3/StoreItem.java
@@ -6,5 +6,27 @@ public class StoreItem {
 	private int itemNo;
 	private String itemDescription;
 	private double price;
+	
+	public StoreItem(int itemNo, String itemDescription, double price) 
+	{	
+		//ensure non-negative item number
+		if(itemNo > 0) {
+			this.itemNo = itemNo;
+		} else {
+			this.itemNo = Math.abs(itemNo);
+		}
+		
+		//ensure non-negative item price
+		if(price >= 0.0) {
+			this.price = price;
+		} else {
+			this.price = 0.00;
+		}
+		
+		this.itemDescription = itemDescription;
+		
+		//increment the class inventory count
+		unitsInInventory++;
+	}
 
 }
