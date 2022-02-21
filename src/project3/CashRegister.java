53c532f5cc4a0c1638df3366e47491ec32f1593b
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-21 17:28:12 -0600

    displayMenu prints available items from list

--- src/project3/CashRegister.java
+++ src/project3/CashRegister.java
@@ -11,5 +11,50 @@ public class CashRegister {
 	{
 		itemsList = new ArrayList<StoreItem>();
 	}
+	
+	public void displayMenu(List<StoreItem> itemsList)
+	{
+		System.out.println("=========");
+		System.out.println("MAIN MENU");
+		System.out.println("=========");
+		
+		System.out.println("Available items:");
+		for(StoreItem item : itemsList) {
+			System.out.println(" - " + item.getItemDescription());
+		}
+		
+		
+	};
+	
+	public double getTotal() 
+	{
+		return 0.0;
+	};
+	
+	public void purchaseItem(StoreItem item) 
+	{
+		
+	};
+	
+	public void showItems()
+	{
+		
+	};
+	
+	private void clearRegister()
+	{
+		
+	};
+	
+	private void showInventory()
+	{
+		
+	};
+	
+	public boolean checkOut()
+	{
+		return true;
+	};
+	
 
 }
