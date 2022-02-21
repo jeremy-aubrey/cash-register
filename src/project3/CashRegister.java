92602e93afc272529b274f4cc72076ac22e339e1
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-21 12:53:17 -0600

    added List member to store StoreItem objects, implemented constructor

--- src/project3/CashRegister.java
+++ src/project3/CashRegister.java
@@ -1,5 +1,15 @@
 package project3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CashRegister {
+	
+	List<StoreItem> itemsList;
+	
+	public CashRegister() 
+	{
+		itemsList = new ArrayList<StoreItem>();
+	}
 
 }
