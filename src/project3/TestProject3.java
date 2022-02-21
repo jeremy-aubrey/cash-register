549a424020a805925cc610e60655aeca9937d3c0
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-21 13:08:37 -0600

    added developerInfo method

--- src/project3/TestProject3.java
+++ src/project3/TestProject3.java
@@ -2,6 +2,28 @@ package project3;
 
 public class TestProject3 {
 	public static void main(String[] args) {
-		System.out.println("Project 3 first commit");
+		
+		TestProject3 obj = new TestProject3();
+		obj.developerInfo();
+		
 	}
+	
+   //***************************************************************
+   //
+   //  Method:       developerInfo (Non Static)
+   // 
+   //  Description:  The developer information method of the program
+   //
+   //  Parameters:   None
+   //
+   //  Returns:      N/A 
+   //
+   //**************************************************************
+   public void developerInfo()
+   {
+      System.out.println("Name:    Jeremy Aubrey");
+      System.out.println("Course:  COSC 4301 Modern Programming");
+      System.out.println("Project: Three\n");
+
+   } // End of the developerInfo method
 }
