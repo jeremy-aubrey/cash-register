e3447ffdbdf4f08516a0bc5117a7befe11f2e409
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-23 23:44:33 -0600

    implemented dispatchAction and selectItems helper methods

--- src/project3/CashRegister.java
+++ src/project3/CashRegister.java
@@ -4,6 +4,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class CashRegister {
@@ -11,17 +12,100 @@ public class CashRegister {
 	private List<StoreItem> selectedItems;
 	private List<StoreItem> inventoryList;
 	private double taxRate = 0.0825;
+	private Scanner userIn = new Scanner(System.in);
 	
-	public CashRegister(List inventoryList) 
+	public CashRegister(List<StoreItem> inventoryList) 
 	{
 		selectedItems = new ArrayList<StoreItem>();
 		this.inventoryList = inventoryList; 
 	}
 	
-	public void displayMenu()
+	public void selectItems()
 	{
+		boolean quit = false;
+		while(!quit) {
+			System.out.println("\n[ " + "SELECT AN ITEM" + " ]");
+			for(int i = 0; i < inventoryList.size(); i++) {
+				StoreItem item = inventoryList.get(i);
+				System.out.printf("%-20s%-5s%n",
+						item.getItemDescription(),
+						"["+(i+1)+"]");
+			}
+			
+			int selection = getUserSelection("\nEnter item: ");
+			int quantity = getUserSelection("Enter quantity: ");
+			
+			if(selection > 0 && selection <= inventoryList.size() && quantity > 0) {
+				
+				StoreItem item = inventoryList.get(selection - 1); //convert to index
+				purchaseItem(item, quantity);
+				
+			} else if (selection < 0 || quantity < 0) {
+				quit = true;
+			}
+			
+		}
+	}
+	
+	public void displayMenu() {
+		boolean quit = false;
+		while(!quit) {
+			
+			System.out.println("\n[ " + "MENU OPTIONS" + " ]");
+			System.out.printf("%-20s%-5s%n%-20s%-5s%n%-20s%-5s%n%-20s%-5s%n%-20s%-5s%n",
+					"Select Items", "[1]",
+					"Show Cash Register", "[2]",
+					"Clear Cash Register", "[3]",
+					"Show Inventory", "[4]",
+					"Check Out", "[5]");
+			quit = dispatchAction();
+		}
 		
 	};
+	
+	private int getUserSelection(String msg) {
+		int selection = 0;
+		System.out.print(msg);
+		try {
+			selection = Integer.parseInt(userIn.nextLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid selection");
+		}
+		
+		return selection;
+	}
+	
+	private boolean dispatchAction() {
+		boolean quit = false;
+		int selection = getUserSelection("\nEnter selection: ");
+		if(selection < 0) selection = 6; //user quits
+		switch(selection) {
+			case 1: 
+				selectItems(); //select items
+				break;
+			case 2: 
+				showItems(); //show cash register
+				break;
+			case 3: 
+				clearRegister(); //clear cash register
+				break;
+			case 4: 
+				showInventory(); //show inventory
+				break;
+			case 5:
+				checkOut();
+				quit = true;
+				break;
+			case 6: //quit
+				System.out.println("Good bye");
+				quit = true;
+				break;
+			default:
+				System.out.println("Enter a number");
+		}
+		
+		return quit;
+	}
 	
 	private void printHeader(String content) {
 		System.out.println("-------------------------------------");
@@ -57,12 +141,15 @@ public class CashRegister {
 		if(item != null) {
 			boolean inStock = decrementInventory(item.getItemNo(), quantity);
 			if(inStock) {
-			selectedItems.add(new StoreItem(item.getItemNo(), 
+				System.out.println("PURCHASING ITEM");
+				selectedItems.add(new StoreItem(item.getItemNo(), 
 					item.getItemDescription(),
 					quantity,
 					item.getPrice()
 					));
-			} 
+			} else {
+				System.out.println("INSUFFICIENT STOCK");
+			}
 		}
 	};
 	
