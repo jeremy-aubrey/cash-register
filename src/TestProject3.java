99bfb55f91e74d59277a832f1f5b57751feb9f08
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-25 20:32:01 -0600

    added module and method comments

--- src/TestProject3.java
+++ src/TestProject3.java
@@ -2,23 +2,19 @@
 //
 //  Developer:     Jeremy Aubrey
 //
-//  Project #:     Project 2 
+//  Project #:     Project 3 
 //
-//  File Name:     ShapeTest.java
+//  File Name:     TestProject3.java
 //
 //  Course:        COSC 4301 - Modern Programming
 //
-//  Due Date:      2/20/2022
+//  Due Date:      2/27/2022
 //
 //  Instructor:    Fred Kumi 
 //
-//  Description:   This program displays the attributes of each shape type,
-//                 including type, dimension type, area, and volume if
-//                 applicable.
-//
-//                 You are allowed to modify only line 84. If you modify
-//                 any other part of the class, you will not receive credit
-//                 for this project.
+//  Description:   A driver for the CashRegister class. The driver is responsible
+//                 for prompting the user for file paths that will be used to populate
+//                 data that will be used by the CashRegister.
 //
 //******************************************************************************
 
@@ -39,39 +35,67 @@ public class TestProject3 {
 	
 	private static Scanner input = new Scanner(System.in);
 	
+   //***************************************************************
+   //
+   //  Method:       main
+   // 
+   //  Description:  The main method of the program
+   //
+   //  Parameters:   String array
+   //
+   //  Returns:      N/A 
+   //
+   //***************************************************************
 	public static void main(String[] args) {
 		
 		TestProject3 obj = new TestProject3();
 		obj.developerInfo();
 		
+		// user provides a path to an item inventory file
 		File inventoryFile = obj.getFile("inventory");
 		List<StoreItem> itemsList = obj.getItems(inventoryFile);
 		
+		// user provides a path to a cashiers list file
 		File cashiersFile = obj.getFile("cashier");
 		List<String> cashierList = obj.getCashiers(cashiersFile);
 		
+		// instantiate a CashRegister object
 		CashRegister register = new CashRegister(itemsList, cashierList);
 		
+		// allow the user to interact using a menu
 		register.displayMenu();
 		
-		input.close();
-	}
+		input.close(); //close scanner
+		
+	}// end main
 
+    //***************************************************************
+    //
+    //  Method:       getFile (Non Static)
+    // 
+    //  Description:  Attempts to get a readable file path from the user.
+    //                Prompts the user with a customizable message.
+    //
+    //  Parameters:   String
+    //
+    //  Returns:      File 
+    //
+    //**************************************************************
 	public File getFile(String label) {
 		
-		File file = null;
-		
+		File file = null; // file to return
+		Path path = null; // path to validate
 		System.out.println("\nEnter an " + label + " file path:");
-		Path path = null;
 	
-		//get legal path
+		// attempt to get a legal file path
 		try {
+			
 			path = Paths.get(input.nextLine().trim());
 			boolean exists = Files.exists(path);
 			boolean isReadable = Files.isReadable(path);
 			boolean isDirectory = Files.isDirectory(path);
 			
-			//instantiate File if its readable
+			//instantiate File if path if valid
 			if(exists && isReadable && !isDirectory) {
 				file = new File(path.toString());
 				System.out.println("Using: " + path);
@@ -82,56 +106,94 @@ public class TestProject3 {
 		}
 		
 		return file;
-	}
+		
+	}// end getFile method
 	
+    //***************************************************************
+    //
+    //  Method:       getItems (Non Static)
+    // 
+    //  Description:  Attempts to populate a new ArrayList with Item 
+    //                objects from data in inventory file. Guaranteed 
+    //                to return a new ArrayList even if its empty
+    //                (unsuccessful file read or null).
+    //
+    //  Parameters:   File
+    //
+    //  Returns:      List<StoreItem>
+    //
+    //**************************************************************
 	public List<StoreItem> getItems(File inventoryFile) {
 		
 		List<StoreItem> itemList = new ArrayList<StoreItem>();
-			
 		BufferedReader reader;
 			
-			try {
-				reader = new BufferedReader(new FileReader(inventoryFile));
-				String line = reader.readLine();
-				while(line != null) {
-					String[] itemData = line.replaceAll("\\s+", ",").split(",");
-					if(itemData.length == 4) {
-						StoreItem item = new StoreItem(
-								Integer.parseInt(itemData[0]), 
-								itemData[1], 
-								Integer.parseInt(itemData[2]), 
-								Double.parseDouble(itemData[3]));
-						
-						itemList.add(item);
-					}
-					line = reader.readLine();
-				}
-				System.out.println("[ " + itemList.size() + " ITEMS UPLOADED ]");
-				
-				if(reader != null) {
-					reader.close();
+		try {
+			
+			reader = new BufferedReader(new FileReader(inventoryFile));
+			String line = reader.readLine();
+			while(line != null) {
+				// generate an array from each line and create a new StoreItem
+				String[] itemData = line.replaceAll("\\s+", ",").split(",");
+				if(itemData.length == 4) {
+					StoreItem item = new StoreItem(
+							Integer.parseInt(itemData[0]), 
+							itemData[1], 
+							Integer.parseInt(itemData[2]), 
+							Double.parseDouble(itemData[3]));
+					
+					itemList.add(item); // add item to the ArrayList
 				}
 				
-			} catch (IOException | NumberFormatException | NullPointerException e) {
-				System.out.print("[ ERROR READING FILE ]: " + e.getMessage() + "\n");
-				System.out.println("[ NO ITEMS UPLOADED ]");
+				line = reader.readLine(); // move to next line
 			}
+			
+			// report number of items uploaded
+			System.out.println("[ " + itemList.size() + " ITEMS UPLOADED ]");
+			
+			if(reader != null) {
+				reader.close();
+			}
+			
+		} catch (IOException | NumberFormatException | NullPointerException e) {
+			
+			System.out.print("[ ERROR READING FILE ]: " + e.getMessage() + "\n");
+			System.out.println("[ NO ITEMS UPLOADED ]");
+		}
 		
 		return itemList;
-	}
+		
+	}// end getItems method
 	
+    //***************************************************************
+    //
+    //  Method:       getCashiers (Non Static)
+    // 
+    //  Description:  Attempts to populate a new ArrayList with Strings 
+    //                (cashier names) from cashiers file. Guaranteed 
+    //                to return a new ArrayList even if its empty
+    //                (unsuccessful file read or null).
+    //
+    //  Parameters:   File
+    //
+    //  Returns:      List<String>
+    //
+    //**************************************************************
 	private List<String> getCashiers(File cashiersFile) {
 		
 		BufferedReader reader = null;
 		List<String> cashierList = new ArrayList<String>();
 		
 		try {
+			
 			reader = new BufferedReader(new FileReader(cashiersFile));
 			String line = reader.readLine();
-			while(line != null) {
-				cashierList.add(line);
-				line = reader.readLine();
+			while(line != null) { 
+				cashierList.add(line); // add String to the ArrayList
+				line = reader.readLine(); // move to next line
 			}
+			
+			// report number of items uploaded
 			System.out.println("[ " + cashierList.size() + " CASHIERS UPLOADED ]");
 			
 			if(reader != null) {
@@ -146,7 +208,7 @@ public class TestProject3 {
 		
 		return cashierList;
 
-	}
+	}// end getCashiers method
 	
    //***************************************************************
    //
@@ -165,5 +227,6 @@ public class TestProject3 {
       System.out.println("Course:  COSC 4301 Modern Programming");
       System.out.println("Project: Three");
 
-   } // End of the developerInfo method
-}
+   }// end developerInfo method
+   
+}// end TestProject3 class
